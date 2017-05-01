411f8d4bc765c716a668d088bca419dcee8fd5ba
celilileri <celil.ileri@bil.omu.edu.tr>
2017-05-02 02:04:46 +0300

    Add dominant color finder

--- lib/colorname.rb
+++ lib/colorname.rb
@@ -1,4 +1,6 @@
+require 'color'
 require 'colorname/version'
+require 'colorname/find'
 
 # Colorname Module
 module Colorname
--- lib/colorname/find.rb
+++ lib/colorname/find.rb
@@ -1,4 +1,21 @@
-# Color name finder module
+# Color Name Finder method
 module Colorname
-  def find(color_code); end
+  module Find
+    # Dominant base color finder
+    def self.Dominant(cc)
+      if cc.red == cc.green && cc.green == cc.blue
+        cc.red < 127 ? :Black : :White
+      # BUGFIX: If only two base color has the same value?
+      else
+        case [cc.red, cc.green, cc.blue].each_with_index.max[1]
+        when 0
+          :Red
+        when 1
+          :Green
+        when 2
+          :Blue
+        end
+      end
+    end
+  end
 end
