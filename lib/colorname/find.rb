404a5c6d61ff70c83f25cc1573527adef9bacf58
celilileri <celil.ileri@bil.omu.edu.tr>
2017-05-05 16:03:21 +0300

    Reconfigure the conditions

--- lib/colorname/find.rb
+++ lib/colorname/find.rb
@@ -1,26 +1,33 @@
 # Color Name Finder method
 module Colorname
-  module Find
-    # Dominant base color finder
-    def self.Dominant(cc)
-      if (cc.red == cc.green) && (cc.green == cc.blue)
-        # XXX Should add grey?
-        cc.red < 127 ? [:Black] : [:White]
-      elsif cc.red == cc.green
-        [:Red, :Green]
-      elsif cc.red == cc.blue
-        [:Red, :Blue]
-      elsif cc.green == cc.blue
-        [:Green, :Blue]
-      else
-        case [cc.red, cc.green, cc.blue].each_with_index.max[1]
-        when 0
-          [:Red]
-        when 1
-          [:Green]
-        when 2
-          [:Blue]
+  # Dominant base color finder
+  class Dominant
+    class << self
+      def base_color(cc)
+        if (cc.red == cc.green) && (cc.green == cc.blue)
+          # XXX Should add grey?
+          cc.red < 127 ? :Black : :White
+        elsif (cc.red == cc.green) && cc.red > cc.blue
+          [:Red, :Green]
+        elsif (cc.red == cc.blue) && cc.red > cc.green
+          [:Red, :Blue]
+        elsif cc.green == cc.blue && cc.green > cc.red
+          [:Green, :Blue]
+        else
+          case [cc.red, cc.green, cc.blue].each_with_index.max[1]
+          when 0
+            :Red
+          when 1
+            :Green
+          when 2
+            :Blue
+          end
         end
+      end
+
+      def find(cc, sign = ', ')        
+        base = base_color(cc)
+        base.is_a?(Array) ? base.join(sign) : base
       end
     end
   end
