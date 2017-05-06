fcbd0c4a283458aab4d8e543c5b37a9d1e4ecd71
celilileri <celil.ileri@bil.omu.edu.tr>
2017-05-06 21:35:09 +0300

    Use make_color for finding color name

--- lib/colorname/find.rb
+++ lib/colorname/find.rb
@@ -1,7 +1,12 @@
 # Color Name Finder method
 module Colorname
   class << self  
-    def find(cc)
+    def find(*cc)
+      decide_color make_color *cc
+    end
+
+    private
+    def decide_color(cc)
       VALUES.select{|_, val| val == [cc.red, cc.green, cc.blue]}.keys
     end
   end
