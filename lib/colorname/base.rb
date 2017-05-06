faefe7c160ce14c8bedf75c69f81724e910733b6
celilileri <celil.ileri@bil.omu.edu.tr>
2017-05-06 22:51:48 +0300

    Update base function

--- lib/colorname/base.rb
+++ lib/colorname/base.rb
@@ -2,29 +2,28 @@
 module Colorname
   class << self 
     def base(*cc)
-      base = base_color make_color *cc
-      base.is_a?(Array) ? base.join(' ') : base
+      base_color make_color *cc
     end
   
     private
     def base_color(cc)
       if cc.red == cc.green && cc.green == cc.blue
         # XXX Should add grey?
-        cc.red < 127 ? :Black : :White
+        cc.red < 127 ? :black : :white
       elsif cc.red == cc.green && cc.red > cc.blue
-        [:Red, :Green]
+        [:red, :green]
       elsif cc.red == cc.blue && cc.red > cc.green
-        [:Red, :Blue]
+        [:red, :blue]
       elsif cc.green == cc.blue && cc.green > cc.red
-        [:Green, :Blue]
+        [:green, :blue]
       else
         case [cc.red, cc.green, cc.blue].each_with_index.max[1]
         when 0
-          :Red
+          :red
         when 1
-          :Green
+          :green
         when 2
-          :Blue
+          :blue
         end
       end
     end
