3bafe76eecf4f1d081854150e1d35c8c90a95a34
celilileri <celil.ileri@bil.omu.edu.tr>
2017-05-13 01:27:02 +0300

    Add method desription

--- lib/colorname.rb
+++ lib/colorname.rb
@@ -7,6 +7,15 @@ require 'colorname/find_by_image'
 require 'colorname/list'
 require 'miro'
 
+##
 # Colorname Module
+#
+# This module helps you to find the closest named color's name.
+#
+# The closest color is finding by calculating percent error formula between
+# your given color and named color list's every element's Red, Green and Blue
+# values.
+# Logically:
+# many named colors = better pair
 module Colorname
 end
